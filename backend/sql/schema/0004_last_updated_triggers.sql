9ee03add8c6852c5cb334808b5718f1ac7716ede
geraldbahati <journeytoharvard@gmail.com>
2024-01-31 12:52:46 +0300

    autogenerated golang configuration changes

--- backend/sql/schema/0004_last_updated_triggers.sql
+++
@@ -1,2 +0,0 @@
--- +goose Up
-CREATE OR REPLACE FUNCTION update_last_updated_column() RETURNS TRIGGER AS $$ BEGIN NEW.last_updated = NOW(); RETURN NEW; END; $$ LANGUAGE plpgsql;
--- backend/sql/schema/0006_alter_refresh_tokens.sql
+++
@@ -1,7 +0,0 @@
--- +goose Up
-ALTER TABLE refresh_tokens
-    ALTER COLUMN created_at SET DEFAULT CURRENT_TIMESTAMP;
-
--- +goose Down
-ALTER TABLE refresh_tokens
-    ALTER COLUMN created_at DROP DEFAULT;
