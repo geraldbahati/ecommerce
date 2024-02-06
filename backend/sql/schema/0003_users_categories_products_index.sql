20573586c29095d484ddf9db08f02b58718e87e0
geraldbahati <journeytoharvard@gmail.com>
2024-02-06 03:49:06 +0300

    Correct migration statements

--- backend/sql/schema/0003_users_categories_products_index.sql
+++ backend/sql/schema/0003_users_categories_products_index.sql
@@ -1,18 +1,51 @@
 -- +goose Up
-CREATE TABLE refresh_tokens (
-    id UUID PRIMARY KEY,
-    user_id UUID NOT NULL,
-    token TEXT NOT NULL,
-    created_at TIMESTAMP WITH TIME ZONE NOT NULL,
-    expires_at TIMESTAMP WITH TIME ZONE NOT NULL,
-    revoked_at TIMESTAMP WITH TIME ZONE,
-    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
-);
+CREATE INDEX idx_categories_name ON categories(name);
 
-CREATE INDEX refresh_tokens_user_id_idx ON refresh_tokens (user_id);
-CREATE INDEX refresh_tokens_token_idx ON refresh_tokens (token);
-CREATE INDEX refresh_tokens_expires_at_idx ON refresh_tokens (expires_at);
-CREATE INDEX refresh_tokens_revoked_at_idx ON refresh_tokens (revoked_at);
+CREATE INDEX idx_products_category_id ON products(category_id);
+CREATE INDEX idx_products_name ON products(name);
+
+CREATE INDEX idx_orders_user_id ON orders(user_id);
+CREATE INDEX idx_orders_order_status ON orders(order_status);
+CREATE INDEX idx_orders_payment_status ON orders(payment_status);
+
+CREATE INDEX idx_shopping_carts_user_id ON shopping_carts(user_id);
+
+CREATE INDEX idx_cart_items_shopping_cart_id ON cart_items(shopping_cart_id);
+CREATE INDEX idx_cart_items_product_id ON cart_items(product_id);
+
+CREATE INDEX idx_order_items_order_id ON order_items(order_id);
+CREATE INDEX idx_order_items_product_id ON order_items(product_id);
+
+CREATE INDEX idx_reviews_user_id ON reviews(user_id);
+CREATE INDEX idx_reviews_product_id ON reviews(product_id);
+
+CREATE INDEX idx_wishlists_user_id ON wishlists(user_id);
+
+CREATE INDEX idx_wishlist_items_wishlist_id ON wishlist_items(wishlist_id);
+CREATE INDEX idx_wishlist_items_product_id ON wishlist_items(product_id);
 
 -- +goose Down
-DROP TABLE refresh_tokens;
+DROP INDEX idx_categories_name;
+
+DROP INDEX idx_products_category_id;
+DROP INDEX idx_products_name;
+
+DROP INDEX idx_orders_user_id;
+DROP INDEX idx_orders_order_status;
+DROP INDEX idx_orders_payment_status;
+
+DROP INDEX idx_shopping_carts_user_id;
+
+DROP INDEX idx_cart_items_shopping_cart_id;
+DROP INDEX idx_cart_items_product_id;
+
+DROP INDEX idx_order_items_order_id;
+DROP INDEX idx_order_items_product_id;
+
+DROP INDEX idx_reviews_user_id;
+DROP INDEX idx_reviews_product_id;
+
+DROP INDEX idx_wishlists_user_id;
+
+DROP INDEX idx_wishlist_items_wishlist_id;
+DROP INDEX idx_wishlist_items_product_id;
